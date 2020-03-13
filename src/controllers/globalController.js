b2ba03be99f80b0807191c5d3ddb32a5c8b410b2
leye195 <leye195@naver.com>
2020-03-13 17:19:06 +0900

    [수정] meta tag 내용 수정 및 추가 함

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const home = (req, res) => {
   const KEY = process.env.MAP_API;
-  res.render("home", { pageTitle: "MaskUp", api: KEY });
+  res.render("home", { api: KEY });
 };
 export const robots = (req, res) => {
   res.type("text/plain");
