e1e9bd331e92a78ee635a81261eab65fbd463a71
leye195 <leye195@naver.com>
2020-03-15 14:21:27 +0900

    [수정] 요일 관련 에러 수정

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,12 +1,12 @@
 import moment from "moment";
 const days = {
+  0: { day: "일", target: "전체" },
   1: { day: "월", target: "1,6" },
   2: { day: "화", target: "2,7" },
   3: { day: "수", target: "3,8" },
   4: { day: "목", target: "4,9" },
   5: { day: "금", target: "5,0" },
-  6: { day: "토", target: "전체" },
-  7: { day: "일", target: "전체" }
+  6: { day: "토", target: "전체" }
 };
 export const localMiddleware = (req, res, next) => {
   const today = moment();
